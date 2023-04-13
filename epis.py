946dfcf52aa651bea5f2204cc6ed9a2eac540e67
Corey Wingo <reposefulGrass@gmail.com>
2023-04-13 05:16:18 -0500

    forward parameters; change window function for compute_L()

--- epis.py
+++ epis.py
@@ -40,7 +40,7 @@ def rgb2lab(pixel_rgb):
     return lab
 
 
-def affinity(pi_lab, pj_lab):
+def affinity(pi_lab, pj_lab, p_kappa, p_sigma):
     """ 
     pi_lab: np.ndarray(3)
     pj_lab: np.ndarray(3)
@@ -48,114 +48,52 @@ def affinity(pi_lab, pj_lab):
     Calculate the affinity between CIELAB pixel_i and pixel_j.
     """
 
-    # constants
-    kappa = 0.3     # sensitivity to luminance variations
-    sigma = 1.0     # metric for pixel affinity
+    pi_lab[0] = pi_lab[0] * p_kappa
+    pj_lab[0] = pj_lab[0] * p_kappa
 
-    pi_lab[0] = pi_lab[0] * kappa
-    pj_lab[0] = pj_lab[0] * kappa
-
-    return np.exp(- (np.square(np.linalg.norm(pi_lab - pj_lab)) / 2 * (sigma ** 2)))
+    return np.exp(- (np.square(np.linalg.norm(pi_lab - pj_lab)) / 2 * (p_sigma ** 2)))
 
 
-# Source: https://gist.github.com/pghazanfari/b23739a0a057e165fa8b591a64ca181d
-def surrounding(x, idx, radius=1, fill=0):
-    """ 
-    Gets surrounding elements from a numpy array 
-  
-    Parameters: 
-    x (ndarray of rank N): Input array
-    idx (N-Dimensional Index): The index at which to get surrounding elements. If None is specified for a particular axis,
-        the entire axis is returned.
-    radius (array-like of rank N or scalar): The radius across each axis. If None is specified for a particular axis, 
-        the entire axis is returned.
-    fill (scalar or None): The value to fill the array for indices that are out-of-bounds.
-        If value is None, only the surrounding indices that are within the original array are returned.
-  
-    Returns: 
-    ndarray: The surrounding elements at the specified index
-    """
-    
-    assert len(idx) == len(x.shape)
-    
-    if np.isscalar(radius): radius = tuple([radius for i in range(len(x.shape))])
-    
-    slices = []
-    paddings = []
-    for axis in range(len(x.shape)):
-        if idx[axis] is None or radius[axis] is None:
-            slices.append(slice(0, x.shape[axis]))
-            paddings.append((0, 0))
-            continue
-            
-        r = radius[axis]
-        l = idx[axis] - r 
-        r = idx[axis] + r
-        
-        pl = 0 if l > 0 else abs(l)
-        pr = 0 if r < x.shape[axis] else r - x.shape[axis] + 1
-        
-        slices.append(slice(max(0, l), min(x.shape[axis], r+1)))
-        paddings.append((pl, pr))
-    
-    if fill is None: return x[slices]
-    return np.pad(x[slices[0], slices[1], slices[2]], paddings, 'constant', constant_values=fill)
-
-
-def local_neighborhood(img, y, x, h):
-    """
-    img: np.ndarray(i, j, 3)
-    y: int < i
-    x: int < j
-    h: int < min(i, j)
-
-    return the (h x h) local neighborhood of pixel_yx.
-    """
-    assert y < img.shape[0], "pixel out of range"
-    assert x < img.shape[1], "pixel out of range"
-    assert h < min(img.shape[0], img.shape[1]), "neighborhood too big"
-
-    return surrounding(img, (y, x, None), (h, h, None))
-
-def compute_L(img, h=11):
+def compute_L(img, p_h, p_kappa, p_sigma):
     img = cp.asnumpy(img)
-    m_l = (2*h + 1) ** 2
+    m_l = (2*p_h + 1) ** 2
     n = img.shape[0] * img.shape[1]
 
     print("m_l:", m_l)
     print("n:", n)
 
-    img_lab = np.zeros_like(img, dtype=float)
+    img_lab = np.zeros_like(img, dtype=np.float64)
     for y1 in range(0, img.shape[0]):
         for x1 in range(0, img.shape[1]):
             img_lab[y1, x1] = rgb2lab(img[y1, x1])
 
-    M = np.zeros((m_l, n), dtype=float)
+    M = np.zeros((m_l, n), dtype=np.float64)
 
     for y1 in range(0, img.shape[0]):
         for x1 in range(0, img.shape[1]):
-            window = local_neighborhood(img_lab, y1, x1, h)
-            middle_y = window.shape[0] // 2
-            middle_x = window.shape[1] // 2
+            pixel_i = img_lab[y1, x1]
+            i = y1 * img.shape[0] + x1
 
-            for y2 in range(0, window.shape[0]):
-                for x2 in range(0, window.shape[1]):
-                    pixel_j = window[y2, x2]
+            ly = y1 - p_h 
+            ry = y1 + p_h
+            lx = x1 - p_h
+            rx = x1 + p_h 
 
-                    # map local neighborhood coordinates to global 
-                    gy2 = y1 - middle_y + y2
-                    gx2 = x1 - middle_x + x2
-                    
-                    k = y2 * window.shape[0] + x2
-                    i = y1 * img.shape[0] + x1
-                    j = gy2 * img.shape[0] + gx2
+            k = 0
+            for y2 in range(ly, ry):
+                for x2 in range(lx, rx):
+                    if y2 > 0 and y2 < img.shape[0] and x2 > 0 and x2 < img.shape[1]:
+                        pixel_j = img_lab[y2, x2]
+                        j = y2 * img.shape[0] + x2
 
-                    if j < img.shape[0] * img.shape[1]:
-                        result = affinity(img_lab[y1, x1], pixel_j)
-                        M[k, i] = result
+                        result = affinity(pixel_i, pixel_j, p_kappa, p_sigma)
+                        #print("k: {}, i: {} - ({}, {}), j: {} - ({}, {}), affinity: {}".format(k, i, y1, x1, j, y2, x2, result))
                         M[k, j] = -result
+                        M[k, i] = result
+                    k += 1
 
-    M = cp.asarray(M)
+    #print("M:", M[:,0])
+    M = cp.asarray(M, dtype=np.float64)
     O = cp.zeros_like(M)
 
     return cp.concatenate(
@@ -192,14 +130,14 @@ def shrink(y, p_gamma):
     return (y / y_norm) * max(y_norm - p_gamma, 0)
 
 
-def image_flatten(img, p_h, p_epsilon, p_beta, p_lambda):
+def image_flatten(img, p_h, p_epsilon, p_beta, p_lambda, p_kappa, p_sigma):
     zin = rgb2z(cp.asarray(img))
     z = cp.zeros((3, zin.shape[0]))
     z[0] = zin
 
     print("zin.shape:", zin.shape)
 
-    L = csr_matrix(compute_L(img, p_h))
+    L = csr_matrix(compute_L(img, p_h, p_kappa, p_sigma))
     d = cp.zeros((3, L.shape[0]))
     b = cp.zeros((3, L.shape[0]))
 
@@ -242,5 +180,11 @@ if __name__ == "__main__":
     img = cp.asarray(cv2.imread(cv2.samples.findFile(filename + filetype)))
     assert img is not None, "file could not be read"
 
-    flat_img = cp.asnumpy(image_flatten(img, 11, 1.0, 2.5, 5.0))
+    p_h = 11
+    p_epsilon = 0.001
+    p_beta = 2.5
+    p_lambda = 5.0
+    p_kappa = 0.3
+    p_sigma = 1.0
+    flat_img = cp.asnumpy(image_flatten(img, p_h, p_epsilon, p_beta, p_lambda, p_kappa, p_sigma))
     cv2.imwrite(filename + "_flattened" + filetype, flat_img)
