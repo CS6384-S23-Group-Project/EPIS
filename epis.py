e35839378c9c203a6fd42289c68c83103e267a46
Corey Wingo <reposefulGrass@gmail.com>
2023-04-14 05:00:48 -0500

    Solved optimization loop increasing

--- epis.py
+++ epis.py
@@ -1,5 +1,5 @@
 
-from cupyx.scipy.sparse import identity, csr_matrix
+from cupyx.scipy.sparse import identity, csr_matrix, hstack, vstack
 from cupyx.scipy.sparse.linalg import spsolve
 import numpy as np
 import cupy as cp
@@ -56,51 +56,55 @@ def affinity(pi_lab, pj_lab, p_kappa, p_sigma):
 
 def compute_L(img, p_h, p_kappa, p_sigma):
     img = cp.asnumpy(img)
-    m_l = p_h ** 2
     n = img.shape[0] * img.shape[1]
+    m_l = n * (p_h ** 2)
 
     img_lab = np.zeros_like(img, dtype=np.float64)
     for y1 in range(0, img.shape[0]):
         for x1 in range(0, img.shape[1]):
             img_lab[y1, x1] = rgb2lab(img[y1, x1])
 
-    M = np.zeros((m_l, n), dtype=np.float64)
+    pair1 = cp.zeros((m_l), dtype=cp.uint32)
+    pair2 = cp.zeros((m_l), dtype=cp.uint32)
+    val = cp.zeros((m_l), dtype=cp.float32)
+    indptr = cp.zeros((m_l), dtype=cp.uint32)
 
-    for y1 in range(0, img.shape[0]):
-        for x1 in range(0, img.shape[1]):
+    k = 0
+    for y1 in range(0, img_lab.shape[0]):
+        for x1 in range(0, img_lab.shape[1]):
             pixel_i = img_lab[y1, x1]
-            i = y1 * img.shape[0] + x1
+            i = y1 * img_lab.shape[0] + x1
 
-            ly = y1 - (p_h // 2 - 1)
-            ry = y1 + (p_h // 2 - 1)
-            lx = x1 - (p_h // 2 - 1)
-            rx = x1 + (p_h // 2 - 1)
-
-            k = 0
-            for y2 in range(ly, ry):
-                for x2 in range(lx, rx):
-                    if y2 > 0 and y2 < img.shape[0] and x2 > 0 and x2 < img.shape[1]:
+            for y2 in range(y1, y1 + p_h):
+                for x2 in range(x1, x1 + p_h):
+                    if y2 < img_lab.shape[0] and x2 < img_lab.shape[1]:
                         pixel_j = img_lab[y2, x2]
                         j = y2 * img.shape[0] + x2
 
-                        result = affinity(pixel_i, pixel_j, p_kappa, p_sigma)
-                        #print("k: {}, i: {} - ({}, {}), j: {} - ({}, {}), affinity: {}".format(k, i, y1, x1, j, y2, x2, result))
-                        M[k, i] = result
-                        M[k, j] = -result
-                    k += 1
+                        pair1[k] = i 
+                        pair2[k] = j 
+                        val[k] = np.linalg.norm(pixel_i - pixel_j)
 
-    #print("M:", M[:,0])
-    M = cp.asarray(M, dtype=np.float64)
-    O = cp.zeros_like(M)
+                        k += 1
 
-    return cp.concatenate(
+    val = np.exp(- np.square(val) / 2 * (p_sigma ** 2))
+
+    M = csr_matrix((val, (pair2, pair1)), shape=(n, m_l))
+    print("n:", n)
+    print("m_l:", m_l)
+    print("M.shape:", M.shape)
+    #M = M.resize((n, m_l))
+    #print("M.shape:", M.shape)
+    O = csr_matrix((n, m_l), dtype=cp.float32)
+    print("O.shape:", O.shape)
+
+    return vstack(
         [
-            cp.concatenate([M, O, O], axis=1), 
-            cp.concatenate([O, M, O], axis=1), 
-            cp.concatenate([O, O, M], axis=1)
-        ], 
-        axis=0
-    )
+            hstack([M, O, O]), 
+            hstack([O, M, O]), 
+            hstack([O, O, M])
+        ],
+    ).T
 
 
 def rgb2z(img):
@@ -127,33 +131,37 @@ def shrink(y, p_gamma):
     return (y / y_norm) * max(y_norm - p_gamma, 0)
 
 
-def image_flatten(img, p_h, p_epsilon, p_beta, p_lambda, p_kappa, p_sigma):
+def image_flatten(img, p_iter, p_h, p_epsilon, p_theta, p_lambda, p_kappa, p_sigma):
     zin = rgb2z(cp.asarray(img))
     z = cp.zeros((3, zin.shape[0]))
     z[0] = zin
 
-    d = cp.zeros((3, 3 * (p_h ** 2)))
-    b = cp.zeros((3, 3 * (p_h ** 2)))
-
-    I = identity(zin.shape[0], dtype=cp.float64, format='csr')
-
-    L = csr_matrix(compute_L(z2rgb(z[0], img.shape[0], img.shape[1]), p_h, p_kappa, p_sigma))
+    L = compute_L(z2rgb(z[0], img.shape[0], img.shape[1]), p_h, p_kappa, p_sigma)
+    print("L.shape:", L.shape)
     LT = L.T
+    print("LT.shape:", LT.shape)
     LTL = LT @ L
+    print("LTL.shape:", LTL.shape)
+
+    d = cp.zeros((3, L.shape[0]))
+    b = cp.zeros((3, L.shape[0]))
+    print("d[0].shape:", d[0].shape)
+
+    I = identity(LTL.shape[0], dtype=cp.float64, format='csr')
 
     i = 0
     print("\n---- OPTIMIZATION ------------------")
-    while (cp.linalg.norm(z[1] - z[0]) ** 2) > p_epsilon:
+    while i < p_iter and cp.linalg.norm(z[1] - z[0]) > p_epsilon:
         print("Iteration {}".format(i))
-        print("difference:", cp.linalg.norm(z[1] - z[0]) ** 2)
+        print("difference:", cp.linalg.norm(z[1] - z[0]))
         print("")
 
-        A = p_beta * I + p_lambda * LTL
-        v = p_beta * zin + cp.asarray(p_lambda * LT @ (d[1] - b[1]))
+        A = p_theta * I + p_lambda * LTL
+        v = p_theta * zin + cp.asarray(p_lambda * LT @ (d[1] - b[1]))
 
         z[2] = spsolve(A, v)
 
-        d[2] = shrink(L @ z[2] + b[1], 1 / p_lambda)
+        d[2] = shrink(L @ z[2] + b[1], 1.0 / p_lambda)
         b[2] = b[1] + L @ z[2] - d[2]
 
         for j in range(0, 2):
@@ -171,11 +179,12 @@ if __name__ == "__main__":
     img = cp.asarray(cv2.imread(cv2.samples.findFile(filename + filetype)))
     assert img is not None, "file could not be read"
 
-    p_h = 15
+    p_iter = 10
+    p_h = 5
     p_epsilon = 0.001
-    p_beta = 2.5
-    p_lambda = 5.0
+    p_theta = 2.5
+    p_lambda = 30
     p_kappa = 0.3
-    p_sigma = 1.0
-    flat_img = cp.asnumpy(image_flatten(img, p_h, p_epsilon, p_beta, p_lambda, p_kappa, p_sigma))
+    p_sigma = 0.5
+    flat_img = cp.asnumpy(image_flatten(img, p_iter, p_h, p_epsilon, p_theta, p_lambda, p_kappa, p_sigma))
     cv2.imwrite(filename + "_flattened" + filetype, flat_img)
